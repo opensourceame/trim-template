0993d37f012647491507bcdedd49d507fbb710d3
David Kelly <david@futuresbright.com>
2024-02-28 12:16:36 +0100

    support for basic IF logic

---
+++ test_if_logic.py
@@ -0,0 +1,26 @@
+import pytest
+from skimpy.skimpy import Skimpy
+
+template = """
+- if True and True and (True or False)
+  h1 Hello World!
+
+- if False
+  h1 Goodbye World!
+
+- if animal == 'dog'
+  h1 Woof!
+
+"""
+
+def test_if_logic():
+    skimpy = Skimpy(template)
+    skimpy.set('animal', 'dog')
+    skimpy.options['pretty'] = False
+    # print(skimpy.render())
+
+    assert skimpy.render() == '<h1>Hello World!</h1><h1>Woof!</h1>'
+
+
+
+test_if_logic()
