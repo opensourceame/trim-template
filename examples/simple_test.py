5c0359fc82e94476ef05c4b85ebba0855b1c390c
David Kelly <david@futuresbright.com>
2025-07-14 18:57:02 +0200

    examples of sub-template rendering

---
+++ examples/simple_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Simple test to verify basic template functionality.
+"""
+
+from trim_template.trim import TrimTemplate
+
+# Simple test with basic variables
+template_vars = {
+    'page_title': 'Test Page',
+    'site_name': 'TestApp',
+    'main_heading': 'Hello World',
+    'content': 'This is a test.',
+    'current_year': 2025,
+    'user': type('User', (), {'logged_in': False})(),
+    'users': [],
+}
+
+def test_simple():
+    tmpl = TrimTemplate('examples/main.html.trim', vars=template_vars, pretty=True)
+    output = tmpl.render()
+    print("=== Simple Test Output ===\n")
+    print(output)
+
+if __name__ == '__main__':
+    test_simple()
